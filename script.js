e86b994fc2131fb8ca38ce6abeac64a9fcb3e0d9
plustamara <tamara.allamm@gmail.com>
2025-09-21 17:38:14 +0300

    My Personal Portolio
    Finished porftolio - done from scratch.

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+    /*Targeting these 02 elements ( menu & icon ) and then 
+    whenever we click it, it's going to either add or remove the open class
+    and this class is going to have some styling ( GIVEN )
+     */
+
+
+
+
